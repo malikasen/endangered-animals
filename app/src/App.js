dfca3c6bf2fb965836f7ee355d75811f76170214
George Song <george@monozuku.com>
2021-04-30 09:11:54 -0700

    feat: add integration with Google Calendar

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 
+import gcal from "react-google-calendar-api";
 import { Routes, Route, Link } from "react-router-dom";
 
 import Tasks from "./Tasks";
@@ -16,18 +17,57 @@ const App = () => (
   </main>
 );
 
-const Home = () => (
-  <>
-    <h1>{process.env.REACT_APP_TITLE}</h1>
-    <h2>{process.env.REACT_APP_SUBTITLE}</h2>
-    <Tasks />
-  </>
-);
+const Home = () => {
+  const [isAuthenticated, setIsAuthenticated] = React.useState(gcal.sign);
+
+  React.useEffect(() => {
+    gcal.onLoad(() => {
+      setIsAuthenticated(gcal.gapi.auth2.getAuthInstance().isSignedIn.get());
+      gcal.listenSign((sign) => setIsAuthenticated(sign));
+    });
+  }, []);
+
+  return (
+    <>
+      <h1>{process.env.REACT_APP_TITLE}</h1>
+      <h2>{process.env.REACT_APP_SUBTITLE}</h2>
+
+      <Login {...{ isAuthenticated }} />
+      {isAuthenticated ? <Events /> : null}
+      <Tasks />
+    </>
+  );
+};
 
 const Dashboard = () => (
   <>
     <h1>Dashboard</h1>
   </>
 );
+
+const Login = ({ isAuthenticated }) =>
+  isAuthenticated ? (
+    <button onClick={gcal.handleSignoutClick}>Log out</button>
+  ) : (
+    <button onClick={gcal.handleAuthClick}>Log in</button>
+  );
+
+const Events = () => {
+  const [events, setEvents] = React.useState([]);
+
+  React.useEffect(() => {
+    gcal
+      .listUpcomingEvents(10)
+      .then(({ result: { items } }) => setEvents(items));
+  }, []);
+
+  return events.length === 0 ? null : (
+    <ul>
+      {events.map((event) => (
+        <li key={event.id}>{event.summary}</li>
+      ))}
+    </ul>
+  );
+};
 
 export default App;
